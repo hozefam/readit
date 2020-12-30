14cb954987428301670ee1706df59ed9f427cd98
Hozefa Mangalorewala <hozefam@Hozefas-MacBook-Pro.local>
2020-12-31 02:14:51 +0300

    Initial Checkin

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+import authRoutes from "./routes/auth";
+import trim from "./middleware/trim";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(trim);
+app.use(cookieParser());
+
+app.get("/", (_, res) => {
+  res.send("Hello World");
+});
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, async () => {
+  console.log(`Server running on http://localhost:5000`);
+  try {
+    await createConnection();
+    console.log("Database Connected");
+  } catch (err) {
+    console.log("Error ", err);
+  }
+});
